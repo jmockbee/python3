f4a056242e49c570f9fa12d0f88c104c1fb8b5e9
Jon Mockbee <mockbee1000@gmail.com>
2021-07-17 15:04:56 -0400

    changing to a string

--- july17.py
+++ july17.py
@@ -6,8 +6,7 @@ print("+" + 10 * "-" + "+")
 
 leg_a = float(input("Input first leg length: "))
 leg_b = float(input("Input second leg length: "))
-hypo = (leg_a**2 + leg_b**2) ** .5
-print("Hypotenuse length is", hypo) 
+print("Hypotenuse length is " + str((leg_a**2 + leg_b**2) ** .5))
 
 
 
