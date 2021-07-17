eec28e88ee309fe2a6830983166eb2fced4b9063
Jon Mockbee <mockbee1000@gmail.com>
2021-07-17 14:57:00 -0400

    Inputs and shapes

---
+++ july17.py
@@ -0,0 +1,14 @@
+print("+" + 10 * "-" + "+")
+print(("#" + " " * 10 + "#\n") * 3, end="")
+print(("#" + "This is" + "#\n" + "#" + "really" + "#\n" +"#"+ "a square" + "#\n"  ), end="")
+print(("#" + " " * 10 + "#\n") * 3, end="")
+print("+" + 10 * "-" + "+")
+
+leg_a = float(input("Input first leg length: "))
+leg_b = float(input("Input second leg length: "))
+hypo = (leg_a**2 + leg_b**2) ** .5
+print("Hypotenuse length is", hypo) 
+
+
+
+
